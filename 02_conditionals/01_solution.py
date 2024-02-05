788309c456a45181726507c9ca1d7df177f7cbf3
Aashish70 <ashishsingh70002@gmail.com>
2024-02-05 23:01:22 +0530

    Add conditional statements to determine age category

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,11 @@
+age = 65
+
+
+if age < 13:
+    print("You are a minor")
+elif age < 18:
+    print("You are a teenager")
+elif age < 50:
+    print("You are an adult") 
+else:
+    print("You are a senior citizen")
